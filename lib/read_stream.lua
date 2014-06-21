f18f3227ca03a990c4521d9cc9cc5f87598e900f
Song Gao <song@gao.io>
2014-06-20 22:31:54 -0500

    add ReadStream

---
+++ lib/read_stream.lua
@@ -0,0 +1,133 @@
+local core = require('core')
+local pathlib = require('path')
+local fs = require('fs')
+
+local Readable = require('../modules/stream').Readable
+
+local ReadOptions = core.Object:extend()
+
+function ReadOptions:initialize()
+  self.flags = 'r'
+  self.mode = '0644'
+  self.offset = 0
+  self.chunk_size = 65536
+  self.fd = nil
+  self.length = nil
+end
+
+local ReadStream = Readable:extend()
+
+function ReadStream:initialize(path, options)
+  Readable.initialize(self)
+
+  self.destroyed = false
+  self.reading = false
+
+  self.path = path
+  self.options = options or ReadOptions:new()
+
+  if not core.instanceof(self.options, ReadOptions) then
+    self:emit('error', core.Error:new("options is not type of ReadOptions"))
+    return
+  end
+  
+  self.offset = self.options.offset
+  if self.options.length ~= nil then
+    self.last = self.options.offset + self.options.length
+  end
+
+  if self.options.fd ~= nil then
+    self.fd = self.options.fd
+  else
+    self:open()
+  end
+end
+
+function ReadStream:open()
+  fs.open(self.path, self.options.flags, self.options.mode, function(er, fd)
+    if er then
+      self:destroy()
+      self:emit('error', er)
+      return
+    end
+    self.fd = fd
+    self:emit('open', fd)
+  end)
+end
+
+function ReadStream:_read(n)
+  if self.fd == nil then
+    self:once('open', function()
+      self:_read(n)
+    end)
+    return
+  end
+
+  if self.destroyed then
+    return
+  end
+
+  if self.reading then
+    process.nextTick(function()
+      self:_read(n)
+    end)
+    return
+  end
+
+  local to_read = self.options.chunk_size
+  if self.last ~= nil then
+    -- indicating length was set in option; need to check boundary
+    if to_read + self.offset > self.last then
+      to_read = self.last - self.offset
+    end
+  end
+
+  self.reading = true
+
+  fs.read(self.fd, self.offset, to_read, function(err, chunk, len)
+    if err or len == 0 then
+      self:destroy(function()
+        self:push(nil)
+      end)
+      if err then
+        self:emit("error", err)
+      end
+      self.reading = false
+    else
+      self:push(chunk)
+      self.offset = self.offset + len
+      self.reading = false
+    end
+  end)
+end
+
+function ReadStream:destroy(callback)
+  if self.destroyed then
+    return
+  end
+  if self.fd then
+    fs.close(self.fd, function(er)
+      if er then
+        self:emit('error', er)
+      else
+        self:emit('close')
+        self:push(nil)
+      end
+    end)
+    self.fd = nil
+    self.destroyed = true
+    callback()
+  else
+    -- fd is not opened yet
+    process.nextTick(function()
+      self:destroy(callback)
+    end)
+  end
+end
+
+local exports = {}
+
+exports.ReadOptions = ReadOptions
+exports.ReadStream = ReadStream
+
+return exports
---
+++ tests/test-read_stream.lua
@@ -0,0 +1,39 @@
+require('helper')
+local test = require('tape')('test ReadStream')
+local fs = require('fs')
+local path = require('path')
+local stream = require('../modules/stream')
+
+local ReadStream = require('../lib/read_stream').ReadStream
+local ReadOptions = require('../lib/read_stream').ReadOptions
+
+local text = [[Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do
+eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+proident, sunt in culpa qui officia deserunt mollit anim id est laborum.]]
+
+local Sink = stream.Writable:extend()
+
+function Sink:initialize()
+  stream.Writable.initialize(self)
+  self.text = ""
+end
+
+function Sink:_write(data, encoding, cb)
+  self.text = self.text .. data
+  cb()
+end
+
+test('read all', nil, function(t)
+  local tmp_file = path.join(__dirname, 'tmp', 'read_all')
+  fs.writeFileSync(tmp_file, text)
+  local rs = ReadStream:new(tmp_file)
+  local sink = Sink:new()
+  sink:once('finish', function()
+    t:equal(text, sink.text, 'incorrect data from ReadStream')
+    t:finish()
+  end)
+  rs:pipe(sink)
+end)
